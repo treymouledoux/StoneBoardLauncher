07c38b7e8a91ab046a3e0b3782a857d6ceb8c243
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2022-09-30 23:13:29 -0500

    changed to power shell

--- Associationsetup.bat
+++ Associationsetup.bat
@@ -1,7 +1,9 @@
-set "PATH=%UTIL_DIR%;%PATH%"
+Set-Variable "PATH=%UTIL_DIR%;%PATH%"
 
 assoc .sbsave=StoneBoardSave
 assoc .sbtheme=StoneBoardTheme
 
 ftype StoneBoardSave="C:\Users\Owner\AppData\Local\Programs\Python\PYTHON36\python.exe" "C:\Users\Owner\Desktop\StoneBoardLauncher-main\importManager.py" "%1" %*
 ftype StoneBoardTheme="C:\Users\Owner\AppData\Local\Programs\Python\PYTHON36\python.exe" "C:\Users\Owner\Desktop\StoneBoardLauncher-main\importManager.py" "%1" %*
+
+Write-Output "Finished successfully!"
